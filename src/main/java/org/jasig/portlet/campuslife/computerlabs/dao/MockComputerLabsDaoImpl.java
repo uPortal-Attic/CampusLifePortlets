2c528e56ac5bc8ff4a1e2c8a8f89d62f02db8e32
Jen Bourey <jennifer.bourey@gmail.com>
2011-12-25 01:49:33 +0000

    Adding computer labs portlet
    git-svn-id: https://source.jasig.org/sandbox/CampusLifePortlets/trunk@25627 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ src/main/java/org/jasig/portlet/campuslife/computerlabs/dao/MockComputerLabsDaoImpl.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.portlet.campuslife.computerlabs.dao;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jasig.portlet.campuslife.computerlabs.model.labs.xml.ComputerLab;
+import org.jasig.portlet.campuslife.computerlabs.model.labs.xml.ComputerLabList;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+
+public class MockComputerLabsDaoImpl implements InitializingBean, IComputerLabsDao {
+    
+    protected final Log log = LogFactory.getLog(getClass());
+    
+    private Resource mockData;
+    
+    @Autowired(required = true)
+    public void setMockData(Resource mockData) {
+        this.mockData = mockData;
+    }
+    
+    private ComputerLabList labs;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(ComputerLabList.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            this.labs = (ComputerLabList) unmarshaller.unmarshal(mockData.getInputStream());
+        } catch (IOException e) {
+            log.error("Failed to read mock data", e);
+        } catch (JAXBException e) {
+            log.error("Failed to unmarshall mock data", e);
+        }
+    }
+
+    @Override
+    public List<ComputerLab> getComputerLabs(PortletRequest request) {
+        return labs.getComputerLabs();
+    }
+
+}
---
+++ src/main/java/org/jasig/portlet/campuslife/computerlabs/mvc/portlet/MainController.java
@@ -0,0 +1,89 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.portlet.campuslife.computerlabs.mvc.portlet;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jasig.portlet.campuslife.computerlabs.dao.IComputerLabsDao;
+import org.jasig.portlet.campuslife.mvc.IViewSelector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+/**
+ * Main portlet view.
+ */
+@Controller
+@RequestMapping("VIEW")
+public class MainController {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+    
+    private IViewSelector viewSelector;
+    
+    @Autowired(required = true)
+    public void setViewSelector(IViewSelector viewSelector) {
+        this.viewSelector = viewSelector;
+    }
+    
+    private IComputerLabsDao computerLabsDao;
+    
+    @Autowired(required = true)
+    public void setComputerLabsDao(IComputerLabsDao computerLabsDao) {
+        this.computerLabsDao = computerLabsDao;
+    }
+    
+    @RenderMapping
+    public ModelAndView showMainView(
+            final RenderRequest request, final RenderResponse response) {
+
+        // determine if the request represents a mobile browser and set the
+        // view name accordingly
+        final boolean isMobile = viewSelector.isMobile(request);
+        final String viewName = isMobile ? "main-jQM" : "main";        
+        final ModelAndView mav = new ModelAndView("computerlabs/" + viewName);
+        
+        if(logger.isDebugEnabled()) {
+            logger.debug("Using view name " + viewName + " for main view");
+        }
+
+        mav.addObject("labs", computerLabsDao.getComputerLabs(request));
+
+        if(logger.isDebugEnabled()) {
+            logger.debug("Rendering main view");
+        }
+
+        return mav;
+
+    }
+
+    @ActionMapping
+    public void doAction() {
+        // no-op action mapping to prevent accidental calls to this URL from
+        // crashing the portlet
+    }
+
+}
